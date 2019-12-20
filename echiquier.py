76ed7354bbf2de7b71ae208107473cbaf4ebc99c
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2019-12-20 12:18:16 +0100

    Add files via upload

---
+++ echiquier.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 17:13:20 2019
+
+@author: 33762
+
+Pour la représentation on a choisi de commencer avec quelque chose de simple
+"""
+import moteur_blocage as mb
+
+def affichage(echiquier):
+    """
+    affichage(List[List[String]])
+    -> return None
+    
+    affiche l'échiquier dans la console python 
+    en donnant les différentes lignes de haut en bas en paramètre
+    WORK
+    """
+    c=['0 ','1 ','2 ','3 ','4 ','5 ','6 ','7 ']
+    tab=['a','z','e','r','t','y','u','i']
+    for i in range(0,len(echiquier)):
+        tab[i]=echiquier[i]
+    premiere_ligne='   '
+    trait=' |____|____|____|____|____|____|____|____|'
+    for i in range(0,len(c)):
+        premiere_ligne=premiere_ligne+c[i]+'   '
+    print(premiere_ligne)
+    print(trait)
+    for i in range(0,len(tab)):
+        ligne = str(i)+'| '
+        for j in range(0,len(tab)):
+            ligne= ligne + tab[i][j] + ' | '
+        ligne=ligne
+        print(ligne)
+        print(trait)
+
+def get_Piece(pos,echiquier):
+    """
+    get_Piece(List[int,int],List[List[String]])
+    -> return string
+    
+    renvoie la pièce qui est sur la position donnée en paramètre 
+    WORK
+    """
+    x=pos[0]
+    y=pos[1]
+    return echiquier[y][x] 
+
+def get_NomPiece(pos,echiquier):
+    """
+    get piece(List[int,int],List[List[String]])
+    -> return string 
+    
+    renvoie le nom de la pièce qui est sur la position donnée en paramètre
+    WORK
+    """
+    piece=get_Piece(pos,echiquier)
+    if 'T' in piece :
+        return 'tour'
+    elif 'C' in piece:
+        return 'cavalier'
+    elif 'F' in piece:
+        return 'fou'
+    elif 'D' in piece:
+        return 'dame'
+    elif 'P' in piece:
+        return 'pion'
+    elif 'R' in piece:
+        return 'roi'
+
+def get_Couleur(pos,echiquier):
+    """
+    get_Couleur(List[int,int],List[List[String]])
+    -> return string
+    
+    retourne la couleur d'une pièce en donnant les coord. de celle-ci'
+    WORK
+    """
+    x=pos[0]
+    y=pos[1]
+    if 'n' in get_Piece([x,y],echiquier):
+        return 'Noir'
+    else :
+        return 'Blanc'
+    
+
+def get_placesDispo_Blancs(echiquier):
+    """
+    get_placeDispoBlancs(List[List[String]])
+    -> return List
+    
+    Cette fonction retourne toutes les places libres pour les blancs
+    on considérant les places occupés par les noirs comme disponibles 
+    WORK
+    """
+    res=[]
+    blancs=['Pb','Tb','Cb','Fb','Rb','Db']
+    for i in range(0,len(echiquier)):
+        for j in range(0,len(echiquier[i])):
+            if echiquier[i][j] not in blancs:
+                res.append([j,i])
+    return res
+
+def get_placesDispo_Noirs(echiquier):
+    """
+    get_placesDispo_Noirs(List[List[String]])
+    -> return List
+    
+    Cette fonction retourne toutes les places libres pour les noirs
+    on considérant les places occupés par les blancs comme disponibles 
+    WORK
+    """
+    res=[]
+    noirs=['Pn','Tn','Cn','Fn','Rn','Dn']
+    for i in range(0,len(echiquier)):
+        for j in range(0,len(echiquier[i])):
+            if echiquier[i][j] not in noirs:
+                res.append([j,i])
+    return res
+
+def initialiseEchiquier():
+    a=['Tn','Cn','Fn','Dn','Rn','Fn','Cn','Tn']
+    z=['Pn','Pn','Pn','Pn','Pn','Pn','Pn','Pn']
+    e=['**','**','**','**','**','**','**','**']
+    r=['**','**','**','**','**','**','**','**']
+    t=['**','**','**','**','**','**','**','**']
+    y=['**','**','**','**','**','**','**','**']
+    u=['Pb','Pb','Pb','Pb','Pb','Pb','Pb','Pb']
+    i=['Tb','Cb','Fb','Db','Rb','Fb','Cb','Tb']
+    echiquier=[a,z,e,r,t,y,u,i]
+    return echiquier
+    
+def choisiDeplacement(echiquier):
+    coups_possibles = []
+    while coups_possibles == []:
+        x,y=demandeUtilisateurPosition()
+        nom=get_NomPiece([x,y],echiquier)
+        if nom=='pion' :
+            coups_possibles = mb.coups_possibles_pion([x,y],echiquier,get_Couleur([x,y],echiquier))
+        elif nom=='cavalier':
+            coups_possibles = mb.coups_possibles_cavalier([x,y],echiquier,get_Couleur([x,y],echiquier))
+        elif nom=='tour':
+            coups_possibles = mb.coups_possibles_tour([x,y],echiquier,get_Couleur([x,y],echiquier))
+        elif nom=='fou':
+            coups_possibles = mb.coups_possibles_fou([x,y],echiquier,get_Couleur([x,y],echiquier))
+        elif nom=='dame':
+            coups_possibles = mb.coups_possibles_dame([x,y],echiquier,get_Couleur([x,y],echiquier))   
+        elif nom=='roi':
+            coups_possibles = mb.coups_possibles_roi([x,y],echiquier,get_Couleur([x,y],echiquier))
+    nbCoup=afficherCoupsPossibles(coups_possibles)
+    choix=demanderChoixUtilisateur(nbCoup)
+    if choix == 0:
+        choisiDeplacement(echiquier)
+    #A faire assoscier le choix a un déplacement et appeler la fonction de déplacement du gourmand !! 
+    
+def afficherCoupsPossibles(coups_possibles):
+    print('0=>Annuler')
+    for i in range(0,len(coups_possibles)):
+        print(str(i+1)+'=>'+str(coups_possibles[i][0])+'-'+str(coups_possibles[i][1]))
+    return i
+
+def demanderChoixUtilisateur(coups_possibles):
+    choix = 999
+    while(choix>coups_possibles+1 or choix<0):
+        choix = int(input("Choisir : ")) #!!! si on rentre un string ca plante il faut régler ca
+    return choix
+
+def demandeUtilisateurPosition():
+    x = int(input("X : "))
+    y = int(input("y : "))
+    return x,y
---
+++ main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 16:51:47 2019
+
+@author: 33762
+"""
+
+import echiquier as ech
+
+#pieces blanches : Pb Cb Fb Tb Rb Db
+#pieces noires : Pn Cn Fn Tn Rn Dn
+
+
+echiquier=ech.initialiseEchiquier()
+ech.affichage(echiquier)
+ech.choisiDeplacement(echiquier)
+
+
+
+
+
+
