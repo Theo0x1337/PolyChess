18dc5d1a17bb94a80cfb300e5acc37a6aa3df3b1
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-01-09 10:41:20 +0100

    Update IA_random.py

--- src/IA_random.py
+++ src/IA_random.py
@@ -21,7 +21,6 @@ def ia_select_dep(echiquier):
     """
     pos_dep=[random.randint(0,7),random.randint(0,7)]
     
-    print("piece choisie : "+str(pos_dep[0])+":"+str(pos_dep[1]))
     if echiquier[pos_dep[1]][pos_dep[0]] == "**" or echiquier[pos_dep[1]][pos_dep[0]][1]!="n":
         return ia_select_dep(echiquier)
     else:
