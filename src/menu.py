99d03ba9bcf29a202887622a2ab3a46ee14c5ad3
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-01-09 10:29:08 +0100

    Update menu.py

--- src/menu.py
+++ src/menu.py
@@ -5,3 +5,12 @@ Created on Thu Jan  9 10:11:59 2020
 @author: 33762
 """
 
+print("============================================")
+print("                PolyChess            ")
+print("============================================")
+print("1->   Joueur vs Joueur")
+print("2->   Joueur vs IA")
+print("3->   IA vs IA")
+mode = 99 
+while(mode>3 or mode<1):
+    mode =int(input("Choisissez votre mode : "))
