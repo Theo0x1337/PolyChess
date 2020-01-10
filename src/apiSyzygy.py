4e4954979cae680567da4e986bd06e366ff57b1b
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-01-10 10:47:05 +0100

    Update apiSyzygy.py

--- src/apiSyzygy.py
+++ src/apiSyzygy.py
@@ -10,11 +10,15 @@ import requests
 
 
 def testFDP(string,couleur):
-    #rnbqkbnr/pp1B1ppp/2p5/4P3/8/6P1/PPPPP2P/RNBQK1NR b - - 0 1
+    """
+    testFDP(string,string) => return Boolean,string
+    Function which calls the syzygy API to test if the game is finished
+    example of string : "rnbqkbnr/pp1B1ppp/2p5/4P3/8/6P1/PPPPP2P/RNBQK1NR b - - 0 1"
+    """
     urlFen = 'https://tablebase.lichess.ovh/standard?fen='+string+"_"+couleur+"_-_-_0_1"
     resp = requests.get(urlFen)
     if resp.status_code == 400 :
-        return True,couleur #a améliorer car c'est un peu de la triche
+        return True,couleur
     return (resp.json()['checkmate']),couleur
 
 
