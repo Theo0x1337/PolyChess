3408a2411fc4ee7508fb1346c9d30e4488f3c4c1
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-01-10 11:20:55 +0100

    Add files via upload

---
+++ src/apiSyzygy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 16:01:23 2020
+
+@author: bernardintheo
+"""
+
+import requests
+
+
+def testFDP(string,couleur):
+    #rnbqkbnr/pp1B1ppp/2p5/4P3/8/6P1/PPPPP2P/RNBQK1NR b - - 0 1
+    urlFen = 'https://tablebase.lichess.ovh/standard?fen='+string+"_"+couleur+"_-_-_0_1"
+    resp = requests.get(urlFen)
+    if resp.status_code == 400 :
+        return True,couleur #a améliorer car c'est un peu de la triche
+    return (resp.json()['checkmate']),couleur
+
+
+
+    
